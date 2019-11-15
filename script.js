a62952142b1abd7c4acfc5c3804f4e0c5ca2e1f2
Maher Wardak <maher.wardak@gmail.com>
2019-11-14 21:46:54 -0500

    Created Title and textbox

---
+++ script.js
@@ -0,0 +1,23 @@
+//A user should be able to see a list of current TO-DO’s
+textBox = "";
+
+
+function toDo(){
+    
+
+
+}
+
+
+
+
+//A user should be able to mark a TO-DO as completed
+
+
+
+
+
+
+
+// //When a user mark’s a TO-DO as completed the message of the TO-DO should be crossed out (using strikethrough font)
+// A user should have the option of deleting
