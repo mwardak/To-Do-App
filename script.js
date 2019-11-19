be5ec15514fe6007fe26201a3cb7b72908874ac6
Maher Wardak <maher.wardak@gmail.com>
2019-11-18 21:00:42 -0500

    created a function to input text enter using appendchild

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 //A user should be able to see a list of current TO-DO’s
-textBox = "";
+   
+    function toDo(){
+    
+    const myList = document.getElementById("myList");
 
+    let newItem = document.createElement("li");
 
-function toDo(){
+    newItem.textContent = "";
+
+    let keyCode = document.addEventListener("keyup", function(){
+    
     
 
-
+    if (keyCode === 13) {
+        newItem = myList.appendChild(newItem);
+        
+    } else {
+        newItem = "";
+    } 
+    });
+  
 }
 
+toDo();
 
-
+  
 
 //A user should be able to mark a TO-DO as completed
 
 
-
-
-
-
-
 // //When a user mark’s a TO-DO as completed the message of the TO-DO should be crossed out (using strikethrough font)
+
+
+
 // A user should have the option of deleting
