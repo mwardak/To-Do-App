f1dcdac1f4b736b57d85145cc6e407bf9cc0226b
Maher Wardak <maher.wardak@gmail.com>
2019-11-19 22:52:17 -0500

    when text is entered a blank OL displays

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 //A user should be able to see a list of current TO-DO’s
-   
-    function toDo(){
-    
-    const myList = document.getElementById("myList");
-
-    let newItem = document.createElement("li");
-
-    newItem.textContent = "";
-
-    let keyCode = document.addEventListener("keyup", function(){
-    
-    
-
-    if (keyCode === 13) {
-        newItem = myList.appendChild(newItem);
-        
-    } else {
-        newItem = "";
-    } 
-    });
   
-}
+  document.addEventListener("keyup", function(e){
+    
+  if (e.keyCode === 13) {
+      const myList = document.getElementById("myList");
+      let newItem = document.createElement("li")
+      newItem = myList.appendChild(newItem).value ==="";
+      newItem.appendChild(document.createTextNode("- "+myList));
 
-toDo();
+  } else {
+      
+  } 
+  });
 
-  
+
+
+
 
 //A user should be able to mark a TO-DO as completed
 
-
 // //When a user mark’s a TO-DO as completed the message of the TO-DO should be crossed out (using strikethrough font)
 
-
-
 // A user should have the option of deleting
+
+
+
+
+
+
+//   let input = document.getElementById("input");
+//   let item = input.value;
+
+//   // ol = document.getElementById("list");
+
+//   let textnode = document.createTextNode(item).appendChild;
+
+//   if (item){
+//       textnode;
+//   } else {
+        
+//   }
+// }
