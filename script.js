7ccad3a0b9d296899b1645c0988e437f7411221f
Maher Wardak <maher.wardak@gmail.com>
2019-11-26 22:13:35 -0500

    textbox is clear after you click enter and deletbtn created

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
 //A user should be able to see a list of current TO-DO’s
 
-  document.addEventListener("keyup", function(e) {
-    if (e.keyCode === 13) {
-      const myList = document.getElementById("myList");
-      let newItem = document.createElement("li");
-      newItem.textContent = "";
+document.addEventListener("keyup", function(e) {
+  if (e.keyCode === 13) {
+    // created a referencec id for the ol element
+    const myList = document.getElementById("myList");
 
-      
+    // creats an li element
+    let newItem = document.createElement("li");
 
-      let inputValue = document.getElementById("enter").value;
-      // newItem.textContent = inputValue;
+    // textcontent for newItem is left empty so user can enter text
+    newItem.textContent = "";
 
-      const span = document.createElement("span");
-      span.className = "item-text";
-      span.textContent = inputValue;
+    //  created a reference id for the input textbox
+    let inputValue = document.getElementById("enter").value;
 
-      myList.appendChild(span);
-    } 
-  });
+    // created the span element
+    const span = document.createElement("span");
+    // created a class reference
+    span.className = "item-text";
 
+    span.textContent = inputValue;
 
+    newItem.appendChild(span);
 
+    myList.appendChild(newItem);
 
-
-//A user should be able to mark a TO-DO as completed
-  myList.addEventListener("click", function(e){
-  
-  e.target.parentElement.className = "done";
-  
-
+    // after you type in text and click enter it should clear the textbox
+    if ((inputValue = document.getElementById("enter").value = "")) {
+      myList.appendChild(newItem) = "";
+    }
+  }
 });
 
-// //When a user mark’s a TO-DO as completed the message of the TO-DO should be crossed out (using strikethrough font)
+//A user should be able to mark a TO-DO as completed
+myList.addEventListener("click", function(e) {
+  if (e) {
+    e.target.parentElement.className = "done";
+  } 
+});
 
 // A user should have the option of deleting
 
-
-
-
+const deleteBtn = document.getElementById("btn");
+deleteBtn.setAttribute("type", "button");
+deleteBtn.addEventListener("click", function(e) {
+  newItem.style = "display: none";
+});
 
 
